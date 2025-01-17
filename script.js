f6312ce2a99a47fdce5c6cefa85ffdfe830e9057
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-01-17 16:06:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let home = ('Sizda hozir rondom sonlar chiqishni boshlaydi');
+alert(home);
+let number = Math.floor(22);
+alert(`Rondom orqali chiqgan son: ${number}`);
+let son = ('Biz rondom orqali chiqgan sonni 2ga kopaytiramiz va bolamiz');
+alert(son);
+
+alert(`Rondom orqali chiqgan son 22\n biz uni 2ga kopaytirib 44 sonini chiqardik \n biz uni 2ga bolib 11 sonini chiqardi\n va shu sonni 3 ga bolib` );
+
+ 
+
+
